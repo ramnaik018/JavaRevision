0a7701f5eb6e1d374a0b636e6ca1211d20268d33
Ram <ramnaikguguloth@Ramnaiks-MacBook-Air.local>
2024-07-31 11:48:35 +0530

    added how to use Comparable to sort

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -1,7 +1,28 @@
+
 package Interfaces;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        List<Student> list=new ArrayList<>();
+        Student s1=new Student("Ram",2);
+        Student s2=new Student("Leela",1);
+        Student s3=new Student("Ravi",3);
+        Student s4=new Student("krishna",4);
+        Student s5=new Student("prem",10);
+        list.add(s1);list.add(s2);list.add(s3);list.add(s4);list.add(s5);
+        System.out.println("Before sorting:" +list);
+        for(Student s:list){
+            System.out.print(s.name +" "+s.rank+" ");
+        }
+        System.out.println();
+        Collections.sort(list);
+        System.out.println("After sorting:");
+        for (Student student : list) {
+            System.out.print(student.name + " "+student.rank+" ");
+        }
+
     }
 }
---
+++ src/Interfaces/Student.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public class Student implements Comparable<Student>{
+    public String name;
+    public int rank;
+
+    public Student(String name,int rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+    public int compareTo(Student s){
+        return this.rank-s.rank;
+    }
+}
